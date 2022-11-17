d0ad56cb909a5c2120b555d84f07dfc3010f5fee
Peace Iyanu Joseph <110996772+Peaceiyanu@users.noreply.github.com>
2022-11-17 17:07:51 +0100

    Update errors1.c

--- errors1.c
+++ errors1.c
@@ -124,6 +124,7 @@ char *convert_number(long int num, int base, int flags)
 /**
  * remove_comments - function replaces first instance of '#' with '\0'
  * @buf: address of the string to modify
+ *
  * Return: Always 0;
  */
 void remove_comments(char *buf)
